8a7e69397450dbd895dc5167a1807f75d6923a00
makehoney <makehoney3@gmail.com>
2018-07-05 05:37:03 +0900

    블록 read and write 구현

--- prototype/src/com/company/Hodadac.java
+++ prototype/src/com/company/Hodadac.java
@@ -2,29 +2,38 @@ package com.company;
 
 import com.hdacSdk.hdacCoreApi.*;
 import com.hdacSdk.hdacWallet.*;
+//import org.apache.commons.codec.DecoderException;
+//import org.apache.commons.codec.binary.Hex;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+
+
 public class Hodadac implements RpcHandler {
-    private HdacWallet hdacWallet;
+    private String txid;
 
     private String rpcIp;
     private String rpcPort;
     private String rpcUser;
     private String rpcPassword;
     private String chainName;
+    private JSONArray userAddr;
+    private JSONArray hotelOwnerAddr;
 
-    public Hodadac(String rpcIp, String rpcPort, String rpcUser,
-                   String rpcPassword, String chainName) {
+    public Hodadac(String rpcIp, String rpcPort, String rpcUser, String rpcPassword,
+                   String chainName, JSONArray userAddr, JSONArray hotelOwnerAddr) {
         this.rpcIp = rpcIp;
         this.rpcPort = rpcPort;
         this.rpcUser = rpcUser;
         this.rpcPassword = rpcPassword;
         this.chainName = chainName;
+        this.userAddr = userAddr;
+        this.hotelOwnerAddr = hotelOwnerAddr;
     }
 
     public void getBalanceOf(JSONArray addr) {
+
         HdacRpcClient rpcClient = createHdacRpcClient(null);
 
         try {
@@ -45,12 +54,78 @@ public class Hodadac implements RpcHandler {
                 }
             });
 
-            hdacCommand.listunspent("0", "999999", addr);
+            hdacCommand.listunspent("0", "99999999", addr);
         } catch(CommandException e) {
             e.printStackTrace();
         }
     }
 
+    /* return transaction hash value */
+    public void writeApplianceInfo(String data) {
+        HdacRpcClient rpcClient = createHdacRpcClient(null);
+
+        try {
+            HdacCommand hdacCommand = new HdacCommand(rpcClient);
+
+            rpcClient.setRPCHandler(new RpcHandler() {
+                @Override
+                public void onResponse(int method, JSONObject data) {
+                    switch (method) {
+                        case CommandUtils.SEND_WITH_DATA_FROM :
+                            System.out.println(data);
+                            /* transaction id 저장 */
+                            txid = data.get("result").toString();
+                            System.out.println(txid);
+                            break;
+                    }
+                }
+
+                @Override
+                public void onError(int method, String err, int i1) {
+                    System.out.print("send error : " + err + "\n");
+                }
+            });
+            hdacCommand.sendwithdatafrom(hotelOwnerAddr.getString(0), userAddr.getString(0),
+                    "0", "byunghun");
+        } catch (CommandException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /* 파라미터 들어가야함(스트링) */
+    public void readApplianceInfo() {
+        HdacRpcClient rpcClient = createHdacRpcClient(null);
+
+        try {
+            HdacCommand hdacCommand = new HdacCommand(rpcClient);
+
+            rpcClient.setRPCHandler(new RpcHandler() {
+                @Override
+                public void onResponse(int method, JSONObject data) {
+                    switch (method) {
+                        case CommandUtils.GET_TXOUT_DATA :
+
+                            /* transaction id 저장 */
+                            System.out.println(data.get("result"));
+                            System.out.println(new String(HdacWallet.hexToByte(data.get("result").toString())));
+                            break;
+                    }
+                }
+
+                @Override
+                public void onError(int method, String err, int i1) {
+                    System.out.print("send error : " + err + "\n");
+                }
+            });
+
+            /* 사실은 txid는 parameter로 들어가야한다. */
+            hdacCommand.gettxoutdata(txid, "1", null, null);
+//            hdacCommand.getrawtransaction(txid, "0");
+        } catch (CommandException e) {
+            e.printStackTrace();
+        }
+
+    }
 
     private HdacRpcClient createHdacRpcClient(RpcHandler handler) {
         HdacRpcClient hdacRpcClient = null;
--- prototype/src/com/company/Main.java
+++ prototype/src/com/company/Main.java
@@ -9,22 +9,22 @@ public class Main {
     private static final String rpcUser = "makehoney";
     private static final String rpcPassword = "1234";
     private static final String chainName = "master";
+    private static final JSONArray userAddr = new JSONArray();
+    private static final JSONArray hotelOwnerAddr = new JSONArray();
 
 
     public static void main(String[] args) {
         try {
-            JSONArray userAddr = new JSONArray();
-            JSONArray hotelOwner = new JSONArray();
-
-            hotelOwner.put("HBCU3nfhmTzSVvPTTUktv1BpdG1CoGY91K");
+            hotelOwnerAddr.put("HBCU3nfhmTzSVvPTTUktv1BpdG1CoGY91K");
             userAddr.put("HKMDrmVWbB9mQRJ2YTs5zWNip68YSAVTLX");
 
-            System.out.println(userAddr.toString());
             Hodadac hodadac = new Hodadac(rpcIp, rpcPort, rpcUser,
-                    rpcPassword, chainName);
+                    rpcPassword, chainName, userAddr, hotelOwnerAddr);
 
-            hodadac.getBalanceOf(userAddr);
-
+//            hodadac.getBalanceOf(userAddr);
+            hodadac.writeApplianceInfo("byunghun");
+            Thread.sleep(10000);
+              hodadac.readApplianceInfo();
             Thread.sleep(100000);
         } catch (InterruptedException e) {
             e.printStackTrace();
